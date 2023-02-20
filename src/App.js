b289e01387dc8027e4a6345e5a1c1ffacb834408
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-02-20 11:12:57 +0100

    add basic html skeleton

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="Calculator"></div>
+      <div className="Calculator">
+        <div className="Header">
+          <h1>Kcalc</h1>
+        </div>
+        <div className="Display">0</div>
+        <div className="ButtonSection">
+          <button className="Button">1</button>
+        </div>
+      </div>
     </div>
   );
 }
