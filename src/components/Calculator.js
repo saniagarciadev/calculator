3b851e7263d58a3345a26c042bce50fa03d778c5
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2023-02-21 22:07:38 +0100

    fix decimal button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,7 +4,7 @@ import NumberButton from "./NumberButton";
 import FunctionButton from "./FunctionButton";
 import "../App.css";
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ","];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "."];
 const operations = ["+", "-", "*", "/", "C", "="];
 
 function Calculator() {
